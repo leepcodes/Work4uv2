6ac7989fb73ee184c1e342fc309ad0218324772f
Josh-Ortega <christianjoshuaortega3@gmail.com>
2026-02-19 10:50:13 +0800

    fix issues

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 // import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Folder } from 'lucide-vue-next';
 import NavFooter from '@/components/NavFooter.vue';
 // import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
